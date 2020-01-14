e53b1606819eb31bb806f3f8ec08bb59880a36de
natefduncan <natefduncan@gmail.com>
2020-01-14 15:52:26 -0600

    file changes

--- bin/util/tidy.sh
+++ bin/util/tidy.sh
@@ -14,7 +14,5 @@ make
 BUILD_DIR=$1
 ln -s $BUILD_DIR/.heroku /app/.heroku
 export PATH=/app/.heroku/php/bin:$PATH
-bash ./install
-cd
 echo "important extension tidy into php.ini"
 echo "extension=tidy.so" >> /app/.heroku/php/etc/php/php.ini
