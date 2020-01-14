458f806bd6206c56afcf64f23a54f5f82dd8af85
natefduncan <natefduncan@gmail.com>
2020-01-14 15:28:58 -0600

    Tidy

--- bin/util/tidy.sh
+++ bin/util/tidy.sh
@@ -1,2 +1,22 @@
-echo "[LOG] Installing TIDY"
-apt install libtidy-dev libtidy5 php-tidy
+apt install cvs
+cvs -d:pserver:anonymous@tidy.cvs.sourceforge.net:/cvsroot/tidy login
+cvs -z3 -d:pserver:anonymous@tidy.cvs.sourceforge.net:/cvsroot/tidy co -P tidy
+echo -e "\n"
+
+$tidy_dir = tidy
+
+#Tidy
+cd $tidy_dr/build/gmake
+make
+
+# /app/php/bin/phpize
+# make
+# make install
+
+BUILD_DIR=$1
+ln -s $BUILD_DIR/.heroku /app/.heroku
+export PATH=/app/.heroku/php/bin:$PATH
+bash ./install
+cd
+echo "important extension tidy into php.ini"
+echo "extension=tidy.so" >> /app/.heroku/php/etc/php/php.ini
