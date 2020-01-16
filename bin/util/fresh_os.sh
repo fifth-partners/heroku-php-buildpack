66fd0a477092938a1297620c23ae7437e5265619
natefduncan <natefduncan@gmail.com>
2020-01-16 08:53:12 -0600

    edits

--- bin/util/fresh_os.sh
+++
@@ -1,6 +0,0 @@
-# Ensure you have read the above information about what this script does before executing these commands.
-
-# Download the script
-wget https://raw.githubusercontent.com/BookStackApp/devops/master/scripts/installation-ubuntu-18.04.sh
-
-./installation-ubuntu-18.04.sh
